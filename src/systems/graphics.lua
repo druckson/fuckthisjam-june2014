3dd9caaeb6e2afa9225d77c6a18a2208c3f09277
Dan Erickson <druckson@gmail.com>
2014-06-04 14:32:05 -0700

    fix camera logic

--- src/systems/graphics.lua
+++ src/systems/graphics.lua
@@ -7,14 +7,62 @@ local Graphics = Class{
     __includes = System,
     init = function(self)
         System.init(self)
-        self.cameraPosition = vector(0, 100)
-        self.scale = 30
+        self.cameraPosition = vector(0, 0)
+        self.cameraRotation = 0
+        self.scale = 50
+        self.fullscreen = false
     end
 }
+
+function Graphics:addEntity(entity, entityData, data)
+    System.addEntity(self, entity, entityData, data)
+    entityData.current.graphics = data.graphics
+    print("Adding graphics "..#self.entities)
+end
 
 function Graphics:config(engine, data)
     self.backgroundColor = data.backgroundColor
     self.foregroundColor = data.foregroundColor
+end
+
+function Graphics:resize(width, height)
+    self.width = width
+    self.height = height
+    love.window.setMode(width, height, {
+        fullscreen = self.fullscreen
+    })
+end
+
+function Graphics:keypressed(key)
+    if key == "f" then
+        self.fullscreen = not self.fullscreen
+        love.window.setMode(love.graphics.getWidth(), love.graphics.getHeight(), {
+            fullscreen = self.fullscreen
+        })
+    end
+end
+
+function Graphics:update(dt)
+    speed = 1
+    turnSpeed = 1
+    if love.keyboard.isDown("w") then
+        self.cameraPosition.y = self.cameraPosition.y + dt*speed
+    end
+    if love.keyboard.isDown("s") then
+        self.cameraPosition.y = self.cameraPosition.y - dt*speed
+    end
+    if love.keyboard.isDown("a") then
+        self.cameraPosition.x = self.cameraPosition.x + dt*speed
+    end
+    if love.keyboard.isDown("d") then
+        self.cameraPosition.x = self.cameraPosition.x - dt*speed
+    end
+    if love.keyboard.isDown("q") then
+        self.cameraRotation = self.cameraRotation + dt*turnSpeed
+    end
+    if love.keyboard.isDown("e") then
+        self.cameraRotation = self.cameraRotation - dt*turnSpeed
+    end
 end
 
 function Graphics:draw()
@@ -25,12 +73,15 @@ function Graphics:draw()
     love.graphics.translate(love.graphics.getWidth()/2, love.graphics.getHeight()/2)
     love.graphics.scale(self.scale, self.scale)
     love.graphics.translate(self.cameraPosition:unpack())
+    love.graphics.rotate(self.cameraRotation)
 
+    love.graphics.setColor(unpack(self.foregroundColor))
     _.each(self.entities, function(entity)
-        love.graphics.translate((-entity.transform.position):unpack())
-        if entity.graphics.type == "circle" then
-            r = entity.graphics.radius
-            love.graphics.circle("fill", -r/2, -r/2, r, 20)
+        love.graphics.translate((entity.current.transform.position):unpack())
+        if entity.current.graphics.type == "circle" then
+            r = entity.current.graphics.radius
+            love.graphics.setColor(unpack(self.foregroundColor))
+            love.graphics.circle("fill", 0, 0, r, 20)
         end
     end)
     love.graphics.pop()
