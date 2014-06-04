f9b7f133f654006f653fa7ac498e00d0796b79a4
Dan Erickson <druckson@gmail.com>
2014-06-04 14:31:20 -0700

    add transform system for data loading

--- src/main.lua
+++ src/main.lua
@@ -1,12 +1,17 @@
 local Engine = require "engine"
+
 local Graphics = require "systems/graphics"
 local Physics = require "systems/physics"
+local Transform = require "systems/transform"
+local MapLoader = require "maploader"
 local _ = require "lib/underscore/lib/underscore"
 
 local engine = Engine()
+local loader = MapLoader(engine)
 
 function love.load()
     -- Create systems
+    engine:addSystem("transform", Transform())
     engine:addSystem("physics", Physics())
     engine:addSystem("graphics", Graphics())
 
@@ -14,14 +19,16 @@ function love.load()
     engine:call("config", engine, {
         worldScale = 1,
         gravity = {0, -10},
-        backgroundColor = {0, 30, 60},
-        foregroundColor = {150, 150, 150}
+        backgroundColor = {0, 30, 60, 255},
+        foregroundColor = {150, 150, 150, 255}
     })
 
     -- Wire up love calls
-    _.each({"update", "draw"}, function(func)
+    _.each({"update", "draw", "resize", "keypressed", "keyreleased"}, function(func)
         love[func] = function(...)
             engine:call(func, ...)
         end
     end)
+
+    loader:loadMap("board1")
 end
---
+++ src/systems/transform.lua
@@ -0,0 +1,20 @@
+local System = require "systems/system"
+local Class = require "lib/hump/class"
+local vector = require "lib/hump/vector"
+
+local Transform = Class{
+    __includes = System,
+    init = function(self)
+        System.init(self)
+    end
+}
+
+function Transform:addEntity(entity, entityData, data)
+    System.addEntity(self, entity, entityData, data)
+    entityData.current.transform = {
+        position = vector(data.transform.position.x, data.transform.position.y),
+        rotation = data.transform.rotation
+    }
+end
+
+return Transform
