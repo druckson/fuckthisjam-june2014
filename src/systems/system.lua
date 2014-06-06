401d450718f3035f8d17b70c7f2cac123fa4a9fd
Dan Erickson <druckson@gmail.com>
2014-06-05 20:30:05 -0700

    fix entity iteration

--- src/systems/system.lua
+++ src/systems/system.lua
@@ -3,15 +3,19 @@ local Class = require "lib/hump/class"
 local System = Class {
     init = function(self)
         self.entities = {}
+        self.entityIDs = {}
     end
 }
 
 function System:addEntity(entity, entityData, data)
-    self.entities[entity] = entityData
+    local entityID = #self.entities
+    self.entityIDs[entity] = entityID
+    table.insert(self.entities, entityData)
 end
 
 function System:removeEntity(entity)
-    self.entites[entity] = nil
+    table.remove(self.entities, self.entityIDs[entity])
+    self.entityIDs[entity] = nil
 end
 
 return System
