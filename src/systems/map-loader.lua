59d4cda01759b375eeea83f8f338f5ced698eaac
Dan Erickson <druckson@gmail.com>
2014-06-13 12:44:11 -0700

    move prefab to utils

--- src/systems/map-loader.lua
+++ src/systems/map-loader.lua
@@ -1,17 +1,6 @@
 local Class = require "lib/hump/class"
 local merge = (require "utils/diff").merge
-
-local function prefab(data, prefabs)
-    local out = {}
-    
-    if data.prefab then
-        if prefabs[data.prefab] then
-            out = prefab(prefabs[data.prefab], prefabs)
-        end
-    end
-
-    return merge(out, data)
-end
+local prefab = require "utils/prefab"
 
 local MapLoader = Class {
     init = function(self)
--- src/systems/map-saver.lua
+++ src/systems/map-saver.lua
@@ -1,18 +1,7 @@
 local Class = require "lib/hump/class"
 local merge = (require "utils/diff").merge
 local diff = (require "utils/diff").diff
-
-local function prefab(data, prefabs)
-    local out = {}
-    
-    if data.prefab then
-        if prefabs[data.prefab] then
-            out = prefab(prefabs[data.prefab], prefabs)
-        end
-    end
-
-    return merge(out, data)
-end
+local prefab = require "utils/prefab"
 
 local MapSaver = Class {
     init = function(self)
